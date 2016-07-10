bfa7bbd1a99ac9aa6c8b0fbaaac3bdb05a0a0094
Jo Van Eyck <ext_jvy@belux.conseur.org>
2016-07-10 11:02:03 +0200

    and a completely random brute-force master with 1000 items just to be on the safe side. Wonder if this one will catch more errors than my coverage-driven manually crafted master!

--- GuildedRose.Tests/GoldenMaster/GoldenMasterTest.cs
+++ GuildedRose.Tests/GoldenMaster/GoldenMasterTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ApprovalTests;
 using ApprovalTests.Reporters;
 using GuildedRose.Console;
@@ -7,8 +9,8 @@ using Xunit;
 
 namespace GuildedRose.Tests.GoldenMaster
 {
-    [UseReporter(typeof(XUnit2Reporter))]
-    //[UseReporter(typeof(DiffReporter))]
+    //[UseReporter(typeof(XUnit2Reporter))]
+    [UseReporter(typeof(DiffReporter))]
     public class GoldenMasterTest
     {
         [Fact]
@@ -33,6 +35,50 @@ namespace GuildedRose.Tests.GoldenMaster
             app.UpdateQuality();
 
             Approvals.VerifyJson(JsonConvert.SerializeObject(app.Items));
-        } 
+        }
+
+        [Fact]
+        public void CompletelyRandomGoldenMaster()
+        {
+            var app = new Program {Items = RandomItems(1000, 1337)};
+
+            app.UpdateQuality();
+
+            Approvals.VerifyJson(JsonConvert.SerializeObject(app.Items));
+        }
+
+        private static List<Item> RandomItems(int numberOfItems, int seed)
+        {
+            var names = 
+                new []
+                {
+                    "_an_item_",
+                    "Aged Brie",
+                    "Backstage passes to a TAFKAL80ETC concert",
+                    "Sulfuras, Hand of Ragnaros"
+                };
+
+            var qualities = Enumerable.Range(0, 51).ToList();
+            var sellIn = Enumerable.Range(0, 100).ToList();
+            
+            var random = new Random(seed);
+
+            return 
+                Enumerable
+                    .Range(1, numberOfItems)
+                    .Select(_ => 
+                        new Item
+                        {
+                            Name = GetRandomFrom(names, random),
+                            Quality = GetRandomFrom(qualities, random),
+                            SellIn = GetRandomFrom(sellIn, random)
+                        })
+                    .ToList();
+        }
+
+        private static T GetRandomFrom<T>(IList<T> source, Random random)
+        {
+            return source[random.Next(source.Count)];
+        }
     }
 }
